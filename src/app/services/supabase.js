5941e3a4bad44306298ca8e2b8b4d0deabe35a26
Fernando Julio <julio8martins@gmail.com>
2025-08-11 21:10:10 -0300

    fix: major errors

--- src/app/services/supabase.js
+++ src/app/services/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const API_KEY = process.env.API_KEY;
-const API_URL = process.env.API_URL;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const supabase = createClient(API_URL, API_KEY);
-
+console.log("Supabase client created with API URL:", API_URL);
 export default supabase;
 
 export async function create(resource, data) {
